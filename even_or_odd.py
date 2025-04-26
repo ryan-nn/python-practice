4a26d9c7d03f37412dffc773b901de4d6660ad1d
ryan-nn <166328879+ryan-nn@users.noreply.github.com>
2025-04-26 14:47:51 +0300

    Add files via upload

---
+++ even_or_odd.py
@@ -0,0 +1,13 @@
+#create a variable x and give it a value
+x=15
+"""process of finding if the number is even or not
+even numbers when divided by 2 the answer should be zero"""
+
+result=x%2
+
+#if statement, if the result is equal to 0 then its even else its odd
+
+if result==0:
+    print(f" The number {x} is an even number ")
+else:
+    print(f" The number {x} is an odd number")
